6afb828fcc2c9ef48fc8ffd732cddd23ab4597d9
Jeremy Ricketts <email@jeremyricketts.com>
2012-10-25 02:31:38 -0700

    Tightened design. Added new code syntax highligher. Coded a code-snippet0expander in js. Started work on first actual post. Made URL's prettier.

--- js/script.js
+++ js/script.js
@@ -7,6 +7,21 @@ $(function() {
 		return false;
 	});
 
+	$('.expandCode a').click( function(){
+		var tthis = $(this);
+		var thePre = $(this).closest('p').prev('pre');
+		if (thePre.hasClass('expanded')) {
+			thePre.removeClass('expanded').width('auto').removeAttr('style');
+			tthis.html('<i class="icon-caret-left"></i> expand <i class="icon-caret-right"></i>');
+		} else {
+			var windowWidth = $(window).width();
+			var leftPos = thePre.position().left;
+			thePre.addClass('expanded').width(windowWidth - 80).css('left', - leftPos + 20);
+			tthis.html('<i class="icon-caret-right"></i> contract <i class="icon-caret-left"></i>');
+		}
+		return false;
+	});
+
 });
 
 
